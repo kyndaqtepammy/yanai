51672901f9c3b9a2e053e17575a2bdc17cdf5e5d
kyndaqtepammy <pamsillah@yahoo.co.uk>
2021-04-21 21:42:18 +0200

    Downloading pdf to internal storage

--- app/src/main/java/com/pamsillah/yanai/MainActivity.java
+++ app/src/main/java/com/pamsillah/yanai/MainActivity.java
@@ -1,9 +1,13 @@
 package com.pamsillah.yanai;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_profile);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(MainActivity.this, item.getItemId(), Toast.LENGTH_SHORT).show();
+        //switch (item.getItemId())
+        return false;
+    }
 }
